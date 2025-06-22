bc3d49da3d3b1c2bb23745f905b505be2e473177
Anshveer Singh <anshveer.singh2023@vitstudent.ac.in>
2025-06-22 12:29:38 +0530

    Create README.d
    Added README for links incase video isn't rendered here.

---
+++ 6. Documentation and Demonstration/Demo Video/README.d
@@ -0,0 +1,10 @@
+## 📽️ Demo Video
+Drive Link :
+```bash
+https://drive.google.com/file/d/1-ui70nAYwWPVB_Gxl-BiDmLoTta7TY5O/view?usp=sharing
+```
+Youtube Link:
+```bash
+https://www.youtube.com/watch?v=gSvXN2rxGD0
+```
+Note: Choose drive link for higher quality.
